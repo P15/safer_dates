731cfa0d6cbc49f850adfb0f4c2c38f08778f8dc
Teddy Widom <teddy@recombine.com>
2015-06-03 15:52:08 -0400

    Add file requires

--- lib/safer_dates/enforcers/attribute_typecasting.rb
+++ lib/safer_dates/enforcers/attribute_typecasting.rb
@@ -1,3 +1,6 @@
+require_relative '../utils/string_utils'
+require_relative '../parsers/date_parser'
+
 ActiveRecord::ConnectionAdapters::Column.class_eval do
   def self.fallback_string_to_date(string)
     return nil if StringUtils.blank?(string)
